10910530269c2f575a76c1481400b0a51930376f
LXG <belsky89@gmail.com>
2022-10-04 22:04:57 +0300

    Added functionality part 11

---
+++ AspNetFrameworkSampleMvcWithIdentity/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AspNetFrameworkSampleMvcWithIdentity.Startup))]
+namespace AspNetFrameworkSampleMvcWithIdentity
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
